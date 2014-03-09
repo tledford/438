948b51f5fee7d97dfa33540315ac73dad025f9aa
Alfredo <alorenzo@csumb.edu>
2014-03-09 15:08:59 -0700

    help me out here guys

--- src/testing.java
+++ src/testing.java
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-
 public class testing {
 	public static void main(String[] args)
 	{
 		System.out.println("test");
 		System.out.println("1... 2... 3...");
 		// how does this program work????
+		// guys. this method is overly complicated.
 	}
-=======
-
-public class testing {
-	public static void main(String[] args)
-	{
-		System.out.println("test");
-	}
->>>>>>> parent of a115865... hey 
 }
