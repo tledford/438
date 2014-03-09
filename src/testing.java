62897f999a0e26baaa4f8002056ae9533f954a07
Ernest <ernhernandez@csumb.edu>
2014-03-09 14:46:10 -0700

    awwww sad

--- src/testing.java
+++ src/testing.java
@@ -3,6 +3,7 @@ public class testing {
 	public static void main(String[] args)
 	{
 		System.out.println("testing");
-		System.out.println("test...icle")
+		System.out.println("test...icle");
+		System.out.println("cancer?");
 	}
 }
