a7a0dad83e6942a42819c8947b69f12140bb2b83
Alfredo <alorenzo@csumb.edu>
2014-03-09 14:56:58 -0700

    h

--- src/testing.java
+++ src/testing.java
@@ -1,5 +1,18 @@
+<<<<<<< HEAD
 
 public class testing {
+	public static void main(String[] args)
+	{
+		System.out.println("test");
+		System.out.println("1... 2... 3...");
+		// how does this program work????
+	}
+=======
 
-	System.out.println("test");
+public class testing {
+	public static void main(String[] args)
+	{
+		System.out.println("test");
+	}
+>>>>>>> parent of a115865... hey 
 }
