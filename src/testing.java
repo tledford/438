343b589dc57aa3a6eff928c46de6f83cae6d3657
Ernest <ernhernandez@csumb.edu>
2014-03-11 16:32:32 -0700

    fixed?

--- src/testing.java
+++ src/testing.java
@@ -18,6 +18,7 @@ public class testing
 public class testing {
 	public static void main(String[] args)
 	{
+<<<<<<< HEAD
 		System.out.println("test");
 		System.out.println("1... 2... 3...");
 		// how does this program work????
@@ -27,6 +28,9 @@ public class testing {
 		//Tommy's test
 		System.out.println("Tommy knows!!");
 		
+=======
+		System.out.println("testing");
+>>>>>>> v1.2
 	}
 }
 >>>>>>> f006958daf54c4fd8ceb6bf2ea8439ffd5e06d51
