24bd071e190ed7bd0eeee8576905a3751eb83998
Ernest <ernhernandez@csumb.edu>
2014-03-11 16:20:13 -0700

    trying to sync

--- src/testing.java
+++ src/testing.java
@@ -1,7 +1,15 @@
 
-public class testing {
+public class testing 
+{
 	public static void main(String[] args)
 	{
-		System.out.println("testing");
+		System.out.println("test");
+		System.out.println("1... 2... 3...");
+		// how does this program work????
+		// guys. this method is overly complicated.
+		System.out.println("Hello World!!");
+		
+		//Tommy's test
+		System.out.println("Tommy knows!!");		
 	}
 }
