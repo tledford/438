4bda3bdfb1a1a69f4eb707799e4db2b2b8a6ad97
Ernest <ernhernandez@csumb.edu>
2014-03-11 15:55:45 -0700

    Ernest commit test

---
+++ testing.java
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+
+public class testing {
+	public static void main(String[] args)
+	{
+		System.out.println("test");
+		System.out.println("1... 2... 3...");
+		// how does this program work????
+	}
+=======
+
+public class testing {
+	public static void main(String[] args)
+	{
+<<<<<<< HEAD
+		System.out.println("testing"); //Ernest
+	}
+=======
+		System.out.println("test");
+	}
+>>>>>>> parent of a115865... hey 
+>>>>>>> a7a0dad83e6942a42819c8947b69f12140bb2b83
+}
