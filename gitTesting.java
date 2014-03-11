f2e361ec59e3c5c893bfe85afa49d8347c2a4758
Alfredo <alorenzo@csumb.edu>
2014-03-11 16:56:34 -0700

    Alfredo - Made some changes

--- gitTesting.java
+++ gitTesting.java
@@ -6,6 +6,23 @@ public class gitTesting {
 		//Is this working??
 		
 		// I don't know
+		
+		HeLLoWorLd();		// can someone help me make this thing easier to read,
+							// i'm trying to make the thing say hello world in the window thing
+	}
+	
+	public void HeLLoWorLd(){
+		int i;
+		String X12FG;
+		X12FG = setStringToHelloWorld();
+		for(i = 0; i < X12FG.length() - 1; i++){
+			System.out.print(X12FG.substring(i, i + 1));
+		}
+	}
+	
+	public void setStringToHelloWorld(){
+		String X12FGTT2 = "Hello World.";
+		return X12FGTT2;
 	}
 
 }
